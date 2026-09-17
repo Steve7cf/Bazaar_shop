9d9a3fa3b23d7bd8669865a0bb6bc753f74a3218
steve bazaar <ice7cf@gmail.com>
2026-09-17 16:02:24 +0300

    removed unused platforms

--- lib/features/home/widgets/home_shell.dart
+++ lib/features/home/widgets/home_shell.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
+// ignore: unused_import
 import '../../../core/theme/app_colors.dart';
 import '../../auth/providers/auth_provider.dart';
 import '../models/nav_item.dart';
@@ -28,7 +30,8 @@ class HomeShell extends ConsumerWidget {
 
     final bottomItems = allNavItems
         .where(
-          (i) => bottomNavRoutes.contains(i.route) && i.visibleTo.contains(role),
+          (i) =>
+              bottomNavRoutes.contains(i.route) && i.visibleTo.contains(role),
         )
         .toList();
 
